5adc1c2a73d12f7c5f9b914ed938422af60ef3d8
Sinan <18sos1@queensu.ca>
2023-03-09 14:54:59 -0500

    heatmap

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import csv
 import collections
 from itertools import chain
-
+import hashlib
 import pandas as pd
 
 csv_data = r'final_mfrc_data.csv'
 read_data = pd.read_csv(csv_data, header=0, names=["text","subreddit","bucket","annotator","annotation","confidence"])
 read_data.head()
+
+'''
 ######preprocessing steps to isolate data we need#########
 list_words = []
 for row in read_data["annotation"]:
@@ -16,7 +18,27 @@ for row in read_data["annotation"]:
     flat_list = list(chain.from_iterable(list_words))
 unique_words = set(flat_list)
 print(collections.Counter(flat_list)) ###visualizaion
+'''
 
+# Faster implementation of the above code #
+
+# Get the unique words
+annotator_data = [{'annotator': row[0], 'annotation': row[1]} for row in read_data[['annotator', 'annotation']].values]
+# Get the counts of each word
+labels = [{'Non-Moral': row['annotation'].count('Non-Moral'), 'Thin Morality': row['annotation'].count('Thin Morality'), 'Care': row['annotation'].count('Care'), 'Equality': row['annotation'].count('Equality'), 'Authority': row['annotation'].count('Authority'), 'Proportionality': row['annotation'].count('Proportionality'), 'Loyalty': row['annotation'].count('Loyalty'), 'Purity': row['annotation'].count('Purity')} for row in annotator_data]
+# Convert to a dataframe
+labels = pd.DataFrame(labels)
+# Sum the counts
+labels = labels.sum(axis=0)
+
+
+hashes = []
+
+for row in read_data["text"]:
+    hashes.append(hashlib.md5(row.encode()).hexdigest()[0:7])
+
+read_data.insert(0,"id", hashes)
+print(read_data.head(100))
 
 
 
